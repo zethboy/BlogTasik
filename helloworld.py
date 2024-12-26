9a6212cd755d51f6dc844300de9be9b38b0d728e
zethboy <yoginugraha842@gmail.com>
2024-12-26 19:33:27 +0700

    pengondisian

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,62 @@
-print("hello world")
+# print("hello world")
+
+# #integer
+# angka = 10
+
+# #float
+# desimal = 3.14
+
+# #string 
+# teks = "hallo nama aku yogi" 
+
+# #boolean 
+# is_phyton_fun = True
+
+# nama = input("siapa nama kamu :")
+# print("nama aku adalah "+ nama +"!")
+
+# if nama == "yogi":
+#   print("kamu gaboleh lanjut")
+# elif nama == "aya":
+#   print ("aya pacar ogii")
+# else:
+#   print ("well")
+
+# a = 5
+# b = 2
+
+# # print(a+b)
+# # print(a-b)
+# # print(a/b)
+# # print(a*b)
+# # print(a**b)
+# # print(a%b)
+
+# looping fir untuk iterasi
+ 
+# for i in range (5):
+#   print("perulangan ke", i)
+
+# while loop
+# i = 0
+# while i<5:
+#   print("perulanagan ke", i)
+#   i += 1
+
+# break dan continue
+
+# for i in range (5):
+#   if i == 3:
+#     break
+#   print (i)
+
+# for i in range (5):
+#   if i == 3:
+#     continue
+#   print (i)
+
+# nested control flow
+
+# for i in range (3):
+#   for j in range (3):
+#     print(f"i={i}, j={j}")
